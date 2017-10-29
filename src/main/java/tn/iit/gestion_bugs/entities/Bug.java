748dcc0aa50d24b1d01602fa466d6a23d93e2471
milamed <medmiledi@gmail.com>
2017-10-29 12:08:25 +0100

    add bug entity

---
+++ src/main/java/tn/iit/gestion_bugs/entities/Bug.java
@@ -0,0 +1,115 @@
+package tn.iit.gestion_bugs.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "bug")
+public class Bug {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private Long id;
+	@Column
+	private String description;
+	@Column
+	private String productVersion;
+	@Column
+	private String detailSteps;
+	@Column
+	private Date dateRaised;
+	@Column
+	private Date dateClosed;
+	@Column
+	private String feedback;
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getProductVersion() {
+		return productVersion;
+	}
+
+	public void setProductVersion(String productVersion) {
+		this.productVersion = productVersion;
+	}
+
+	public String getDetailSteps() {
+		return detailSteps;
+	}
+
+	public void setDetailSteps(String detailSteps) {
+		this.detailSteps = detailSteps;
+	}
+
+	public Date getDateRaised() {
+		return dateRaised;
+	}
+
+	public void setDateRaised(Date dateRaised) {
+		this.dateRaised = dateRaised;
+	}
+
+	public Date getDateClosed() {
+		return dateClosed;
+	}
+
+	public void setDateClosed(Date dateClosed) {
+		this.dateClosed = dateClosed;
+	}
+
+	public String getFeedback() {
+		return feedback;
+	}
+
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bug other = (Bug) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+
+}
