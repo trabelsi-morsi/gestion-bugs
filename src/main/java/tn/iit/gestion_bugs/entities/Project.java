ef337723dfbc090aa4af7649ce5ac2a66ff06683
yaichZied <zied.yaich5@gmail.com>
2017-10-29 11:10:30 +0000

    Severity and Project entities added

--- src/main/java/tn/iit/gestion_bugs/entities/Project.java
+++ src/main/java/tn/iit/gestion_bugs/entities/Project.java
@@ -1,6 +1,7 @@
 package tn.iit.gestion_bugs.entities;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,8 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Severity implements Serializable{
-	
+public class Project implements Serializable{
 	/**
 	 * 
 	 */
@@ -24,40 +24,31 @@ public class Severity implements Serializable{
 	@Column
 	private String description;
 	@Column
-	private String color;
-
+	private Date date;
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 	public String getDescription() {
 		return description;
 	}
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
-	public String getColor() {
-		return color;
+	public Date getDate() {
+		return date;
 	}
-
-	public void setColor(String color) {
-		this.color = color;
+	public void setDate(Date date) {
+		this.date = date;
 	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -65,7 +56,6 @@ public class Severity implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -74,7 +64,7 @@ public class Severity implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Severity other = (Severity) obj;
+		Project other = (Project) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -82,6 +72,5 @@ public class Severity implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
+
 }
